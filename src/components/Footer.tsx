c5eb2f7043d5aac80b894695d8a959eb56f22abf
Leonardo_Daitx <leonnardomelo3@gmail.com>
2023-10-09 15:06:10 -0300

    Footer edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <p>
-        <span>React + TS Todo</span> @ 2021
+        <span>React + TS Todo</span> @ 2023
       </p>
     </footer>
   );
